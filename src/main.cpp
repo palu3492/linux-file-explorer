d684798d2012db1fc3fc2289d6ff7c0b65b1e0da
Alex Palumbo <palu3492@stthomas.edu>
2020-05-18 00:37:55 -0500

    Paging working

--- src/main.cpp
+++ src/main.cpp
@@ -33,18 +33,31 @@ typedef struct Paging {
     int start;
     int end;
     int page_size;
+    int next_click_area[4];
+    int prev_click_area[4];
+    SDL_Texture *next_texture;
+    SDL_Texture *prev_texture;
+    int next_texture_width;
+    int next_texture_height;
+    int prev_texture_width;
+    int prev_texture_height;
+    int number_of_files;
 } Paging;
 
 void initialize_renderer(SDL_Renderer *renderer, TTF_Font **font);
 void changeDirectory(std::vector<FileEntry*> &file_list, const char *path, SDL_Renderer *renderer, TTF_Font *font, SDL_Color color, Paging *paging);
 std::vector<FileEntry*> loadDirectoryFiles(const char *dirname, Paging *paging);
-void createFilenameTextures(std::vector<FileEntry*> &file_list, TTF_Font *font, SDL_Color text_color, SDL_Renderer *renderer);
+void changePageInfo(Paging *paging);
+void createFilenameTextures(std::vector<FileEntry*> &file_list, TTF_Font *font, SDL_Color text_color, SDL_Renderer *renderer, Paging *paging);
 void renderFiles(SDL_Renderer *renderer, std::vector<FileEntry*> &file_list, Paging *paging);
+void createPagingButtons(SDL_Renderer *renderer, Paging *paging, TTF_Font *font);
 void renderPaging(SDL_Renderer *renderer, Paging *paging, TTF_Font *font);
 void mousePress(int mouse_x, int mouse_y, uint8_t button, SDL_Renderer *renderer, std::vector<FileEntry*> &file_list, TTF_Font *font, SDL_Color color, Paging *paging);
 int drawHighlight(int mouse_x, int mouse_y, SDL_Renderer *renderer, std::vector<FileEntry*> &file_list, int highlighted, Paging *paging);
 bool fileEntryComparator(const FileEntry *a, const FileEntry *b);
-void drawRect(SDL_Renderer *renderer, FileEntry* file, SDL_Color color);
+void drawRectFile(SDL_Renderer *renderer, FileEntry* file, SDL_Color color);
+void drawRectButton(SDL_Renderer *renderer, int button, SDL_Color color);
+void drawRectButton(SDL_Renderer *renderer, int button, SDL_Color color, Paging *paging);
 
 int main(int argc, char **argv)
 {
@@ -63,12 +76,13 @@ int main(int argc, char **argv)
     TTF_Font *font;
     initialize_renderer(renderer, &font);
 
+    // Setup paging
     Paging *paging = new Paging();
-    paging->pages = 0;
     paging->current_page = 0;
-    paging->start = 0;
-    paging->end = 0;
     paging->page_size = 10;
+
+    // Create paging button textures
+    createPagingButtons(renderer, paging, font);
 
     // Start in home directory
     std::vector<FileEntry*> file_list;
@@ -128,10 +142,10 @@ void changeDirectory(std::vector<FileEntry*> &file_list, const char *path, SDL_R
     // get list of files in directory stored in FileEntry structs
     file_list = loadDirectoryFiles(path, paging);
     // create textures for file name text
-    createFilenameTextures(file_list, font, color, renderer);
+    createFilenameTextures(file_list, font, color, renderer, paging);
     // Renders the file icon and name to window
     renderFiles(renderer, file_list, paging);
-
+    // render paging elements
     renderPaging(renderer, paging, font);
 }
 
@@ -168,20 +182,30 @@ std::vector<FileEntry*> loadDirectoryFiles(const char *dirname, Paging *paging) 
     }
 
     std::sort(result.begin(), result.end(), fileEntryComparator);
-
-    int start = paging->current_page * paging->page_size;
-    int end = start + paging->page_size;
-    int file_list_size = result.size();
-    if(end > file_list_size){
-        end = file_list_size;
+    
+    paging->current_page = 0;
+    paging->number_of_files = result.size();
+    changePageInfo(paging);
+    paging->pages = paging->number_of_files / paging->page_size;
+    if(paging->number_of_files % paging->page_size != 0){
+        paging->pages++;
     }
-    paging->start = start;
-    paging->end = end;
 
     return result;
 }
 
-void createFilenameTextures(std::vector<FileEntry*> &file_list, TTF_Font *font, SDL_Color text_color, SDL_Renderer *renderer) {
+void changePageInfo(Paging *paging){
+    int start = paging->current_page * paging->page_size;
+    int end = start + paging->page_size;
+
+    if(end > paging->number_of_files){
+        end = paging->number_of_files;
+    }
+    paging->start = start;
+    paging->end = end;
+}
+
+void createFilenameTextures(std::vector<FileEntry*> &file_list, TTF_Font *font, SDL_Color text_color, SDL_Renderer *renderer, Paging *paging) {
     int i;
     for (i = 0; i < file_list.size(); i++) {
         SDL_Surface *surface = TTF_RenderText_Solid(font, file_list[i]->name.c_str(), text_color);
@@ -190,9 +214,10 @@ void createFilenameTextures(std::vector<FileEntry*> &file_list, TTF_Font *font, 
         file_list[i]->texture_height = surface->h;
         SDL_FreeSurface(surface);
 
-        file_list[i]->click_area[0] = 40; // x1
-        file_list[i]->click_area[1] = 42 + (i * 50);
-        file_list[i]->click_area[2] = 55 + file_list[i]->texture_width; // x2
+        file_list[i]->click_area[0] = 0; // x1
+        int y = i % paging->page_size;
+        file_list[i]->click_area[1] = 42 + (y * 50);
+        file_list[i]->click_area[2] = WIDTH; // x2
         file_list[i]->click_area[3] = 40;
 
     }
@@ -209,7 +234,8 @@ void renderFiles(SDL_Renderer *renderer, std::vector<FileEntry*> &file_list, Pag
     for (i = paging->start; i < paging->end; i++) {
         // icon
         rect.x = 45;
-        rect.y = 45 + (i * 50);
+        int y = i % paging->page_size;
+        rect.y = 45 + (y * 50);
         rect.w = 33;
         rect.h = 33;
         const char *file;
@@ -225,7 +251,8 @@ void renderFiles(SDL_Renderer *renderer, std::vector<FileEntry*> &file_list, Pag
 
         // text
         rect.x = 85;
-        rect.y = 50 + (i * 50);
+        y = i % paging->page_size;
+        rect.y = 50 + (y * 50);
         rect.w = file_list[i]->texture_width;
         rect.h = file_list[i]->texture_height;
         SDL_RenderCopy(renderer, file_list[i]->name_texture, NULL, &rect);
@@ -233,6 +260,36 @@ void renderFiles(SDL_Renderer *renderer, std::vector<FileEntry*> &file_list, Pag
 
     // show rendered frame
     SDL_RenderPresent(renderer);
+}
+
+void createPagingButtons(SDL_Renderer *renderer, Paging *paging, TTF_Font *font) {
+    SDL_Color text_color = {255, 255, 255};
+    // Next
+    std::string next_name = "Next Page";
+    SDL_Surface *surface = TTF_RenderText_Solid(font, next_name.c_str(), text_color);
+    paging->next_texture = SDL_CreateTextureFromSurface(renderer, surface);
+    paging->next_texture_width = surface->w;
+    paging->next_texture_height = surface->h;
+
+    // Prev
+    std::string prev_name = "Previous Page";
+    surface = TTF_RenderText_Solid(font, prev_name.c_str(), text_color);
+    paging->prev_texture = SDL_CreateTextureFromSurface(renderer, surface);
+    paging->prev_texture_width = surface->w;
+    paging->prev_texture_height= surface->h;
+
+    SDL_FreeSurface(surface);
+
+    // Click areas
+    paging->next_click_area[0] = WIDTH - 30 - paging->next_texture_width; // x1
+    paging->next_click_area[1] = HEIGHT - 50;
+    paging->next_click_area[2] = paging->next_texture_width; // x2
+    paging->next_click_area[3] = paging->next_texture_height;
+
+    paging->prev_click_area[0] = 30; // x1
+    paging->prev_click_area[1] = HEIGHT - 50;
+    paging->prev_click_area[2] = paging->prev_texture_width; // x2
+    paging->prev_click_area[3] = paging->prev_texture_height;
 }
 
 void renderPaging(SDL_Renderer *renderer, Paging *paging, TTF_Font *font) {
@@ -247,7 +304,7 @@ void renderPaging(SDL_Renderer *renderer, Paging *paging, TTF_Font *font) {
 
     // text
     SDL_Color text_color = {255, 255, 255};
-    std::string text = "Page " + std::to_string(paging->current_page + 1);
+    std::string text = "Page " + std::to_string(paging->current_page + 1) + "/" + std::to_string(paging->pages);
     SDL_Surface *surface = TTF_RenderText_Solid(font, text.c_str(), text_color);
     SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
     SDL_FreeSurface(surface);
@@ -258,6 +315,19 @@ void renderPaging(SDL_Renderer *renderer, Paging *paging, TTF_Font *font) {
     rect.h = surface->h;
 
     SDL_RenderCopy(renderer, texture, NULL, &rect);
+
+    // Buttons
+    rect.x = 30;
+    rect.y = HEIGHT - 50;
+    rect.w = paging->prev_texture_width;
+    rect.h = paging->prev_texture_height;
+    SDL_RenderCopy(renderer, paging->prev_texture, NULL, &rect);
+
+    rect.x = WIDTH - 30 - paging->next_texture_width;
+    rect.y = HEIGHT - 50;
+    rect.w = paging->next_texture_width;
+    rect.h = paging->next_texture_height;
+    SDL_RenderCopy(renderer, paging->next_texture, NULL, &rect);
 
     // show rendered frame
     SDL_RenderPresent(renderer);
@@ -276,6 +346,33 @@ void mousePress(int mouse_x, int mouse_y, uint8_t button, SDL_Renderer *renderer
             break;
         }
     }
+    if (mouse_x >= paging->next_click_area[0] &&
+        mouse_x <= paging->next_click_area[0] + paging->next_click_area[2] &&
+        mouse_y >= paging->next_click_area[1] &&
+        mouse_y <= paging->next_click_area[1] + paging->next_click_area[3])
+    {
+        if(paging->current_page < paging->pages - 1) {
+            paging->current_page++;
+            changePageInfo(paging);
+            // Renders the file icon and name to window
+            renderFiles(renderer, file_list, paging);
+            // render paging elements
+            renderPaging(renderer, paging, font);
+        }
+    } else if (mouse_x >= paging->prev_click_area[0] &&
+               mouse_x <= paging->prev_click_area[0] + paging->prev_click_area[2] &&
+               mouse_y >= paging->prev_click_area[1] &&
+               mouse_y <= paging->prev_click_area[1] + paging->prev_click_area[3])
+    {
+        if(paging->current_page > 0) {
+            paging->current_page--;
+            changePageInfo(paging);
+            // Renders the file icon and name to window
+            renderFiles(renderer, file_list, paging);
+            // render paging elements
+            renderPaging(renderer, paging, font);
+        }
+    }
 }
 
 int drawHighlight(int mouse_x, int mouse_y, SDL_Renderer *renderer, std::vector<FileEntry*> &file_list, int highlighted, Paging *paging) {
@@ -288,21 +385,46 @@ int drawHighlight(int mouse_x, int mouse_y, SDL_Renderer *renderer, std::vector<
         {
             if(i != highlighted){
                 SDL_Color color = {240, 119, 70};
-                drawRect(renderer, file_list[i], color);
+                drawRectFile(renderer, file_list[i], color);
             }
             return i;
         }
     }
-    if(highlighted != -1){
+    if (mouse_x >= paging->next_click_area[0] &&
+        mouse_x <= paging->next_click_area[0] + paging->next_click_area[2] &&
+        mouse_y >= paging->next_click_area[1] &&
+        mouse_y <= paging->next_click_area[1] + paging->next_click_area[3])
+    {
+        if(highlighted != -2){
+            SDL_Color color = {255, 255, 255};
+            drawRectButton(renderer, 0, color, paging);
+        }
+        return -2;
+    } else if (mouse_x >= paging->prev_click_area[0] &&
+               mouse_x <= paging->prev_click_area[0] + paging->prev_click_area[2] &&
+               mouse_y >= paging->prev_click_area[1] &&
+               mouse_y <= paging->prev_click_area[1] + paging->prev_click_area[3])
+    {
+        if(highlighted != -3){
+            SDL_Color color = {255, 255, 255};
+            drawRectButton(renderer, 1, color, paging);
+        }
+        return -3;
+    }
+    if(highlighted > -1){
         for (i = paging->start; i < paging->end; i++) {
             SDL_Color color = {255, 255, 255};
-            drawRect(renderer, file_list[i], color);
+            drawRectFile(renderer, file_list[i], color);
         }
+    } else if(highlighted < -1) {
+        SDL_Color color = {96, 93, 83};
+        drawRectButton(renderer, 0, color, paging);
+        drawRectButton(renderer, 1, color, paging);
     }
     return -1;
 }
 
-void drawRect(SDL_Renderer *renderer, FileEntry* file, SDL_Color color){
+void drawRectFile(SDL_Renderer *renderer, FileEntry* file, SDL_Color color){
     SDL_Rect rect;
     rect.x = -1;
     rect.y = file->click_area[1];
@@ -310,6 +432,24 @@ void drawRect(SDL_Renderer *renderer, FileEntry* file, SDL_Color color){
     rect.h = file->click_area[3];
     SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 255);
 
+    SDL_RenderDrawRect(renderer, &rect);
+    SDL_RenderPresent(renderer);
+}
+
+void drawRectButton(SDL_Renderer *renderer, int button, SDL_Color color, Paging *paging){
+    SDL_Rect rect;
+    if(button == 0){
+        rect.x = paging->next_click_area[0] - 5;
+        rect.y = paging->next_click_area[1] - 5;
+        rect.w = paging->next_click_area[2] + 10;
+        rect.h = paging->next_click_area[3] + 10;
+    } else if(button == 1){
+        rect.x = paging->prev_click_area[0] - 5;
+        rect.y = paging->prev_click_area[1] - 5;
+        rect.w = paging->prev_click_area[2] + 10;
+        rect.h = paging->prev_click_area[3] + 10;
+    }
+    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 255);
     SDL_RenderDrawRect(renderer, &rect);
     SDL_RenderPresent(renderer);
 }
